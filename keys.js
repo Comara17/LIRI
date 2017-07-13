c65b31af10529c31f7fb30ad3014d18875bba35b
Chris O'Mara <comara17@gmail.com>
2017-07-13 17:34:10 -0400

    initial commit

---
+++ keys.js
@@ -0,0 +1,15 @@
+exports.twitterKeys = {
+  consumer_key: 'F57ntRyBM7gJew3eCQkkaHxrk',
+  consumer_secret: 'NXyWpXy7PnrFxbYhSm3wJsxPSrBsRRoV6nfNuT5pVb9ErEhF0f',
+  access_token_key: '885527131937288192-xDiJhi2WDHdLqDibVoA6CPFJpLf5uIY',
+  access_token_secret: 'BoxkHn2DVSybyKTGZz9ByUCxfhb6XBXJYYXjks20el4Fk',
+}
+
+exports.spotifyKeys = {
+	id: '2cec42af3b434b939a530163415b2952',
+	secret: '142401dfbac7419aaaeca1f07d8363fd',
+}
+
+exports.imdbKeys = {
+	key: '40e9cece',
+}
